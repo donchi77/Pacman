9dc691ffb7d828a70d85382b337d2b206b5fb47a
Rigamonti Andrea <DonChisciotte77@users.noreply.github.com>
2020-01-24 10:37:51 +0100

    Modifying Class Connection

---
+++ src/Connection/UDPClient.java
@@ -0,0 +1,10 @@
+package Connection;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPClient {
+    private DatagramSocket datagram;
+    private InetAddress serverAddress;
+    private int port;
+}
