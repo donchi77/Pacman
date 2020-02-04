30cf9d315f3536c147fa1c14451675556323053c
Rigamonti Andrea <DonChisciotte77@users.noreply.github.com>
2020-02-04 11:56:34 +0100

    Modifying Class Connection

--- src/Connection/UDPClient.java
+++ src/Connection/UDPClient.java
@@ -1,14 +1,35 @@
 package Connection;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.*;
 
 public class UDPClient {
     private DatagramSocket datagram;
     private InetAddress serverAddress;
     private int port;
 
-    public void connectToUDPServer() {
+    public UDPClient(String destination, int port) {
+        try {
+            this.serverAddress = InetAddress.getByName(destination);
+            this.port = port;
+            this.datagram = new DatagramSocket(this.port);
+        } catch (SocketException | UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendPosition(int x, int y) {
+        String posX = String.valueOf(x);
+        String posY = String.valueOf(y);
+        try {
+            datagram.send(new DatagramPacket(posX.getBytes(), posX.getBytes().length, serverAddress, port));
+            datagram.send(new DatagramPacket(posY.getBytes(), posY.getBytes().length, serverAddress, port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getPositions() {
 
     }
 }
