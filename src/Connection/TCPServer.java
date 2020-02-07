a7b94e8c317004547fa648a4e3799db22afbf674
Farina Simone <farinasimone2001@gmail.com>
2020-02-07 12:49:13 +0100

    Server

--- src/Connection/TCPServer.java
+++ src/Connection/TCPServer.java
@@ -1,19 +1,25 @@
 package Connection;
 
+import Players.Player;
+import Players.Players;
+
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TCPServer {
     //Variabili globali
-    static Socket[] socketD = new Socket[4];
-    static TCPServer server;
+    Socket[] socketD = new Socket[4];
+    //Player player;
+    Players players;
+    ObjectInputStream objInput;
 
     public TCPServer(){
-        server = new TCPServer();
     }
 
-    public static void main(){
+    public static void main(String[] args) {
         try{
             System.out.println("-------------");
             System.out.println("Server PacMan");
@@ -21,20 +27,30 @@ public class TCPServer {
 
             //creazione della connection socket sulla porta 8080
             System.out.println("Creazione della socket...");
-            ServerSocket socketC = new ServerSocket(8080);
 
+            ServerSocket socketC = new ServerSocket(8080);
+            TCPServer server = new TCPServer();
             server.acc(socketC);
+            server.game();
         } catch(Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public static void acc(ServerSocket socketC){
+    private void game() {
+        System.out.println("Tutti i giocatori necessari connessi");
+
+
+    }
+
+    public void acc(ServerSocket socketC){
         //aspettando la accept
         System.out.println("Aspettando la connessione di tutti i player...");
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 4; i++) {
             try {
                 socketD[i] = socketC.accept();
+                objInput = new ObjectInputStream(new BufferedInputStream(socketD[i].getInputStream()));
+                // TODO player objects
             } catch (IOException e) {
                 e.printStackTrace();
             }
